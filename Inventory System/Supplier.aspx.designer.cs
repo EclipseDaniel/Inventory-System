467eb89cfeaee9aec24c41cf6fd8ce5ffdb3a11c
Galang, Eldon Marc N <eldonmarcgalang@gmail.com>
2019-06-13 19:36:04 +0800

    Error In Supplier Page-Outlook Latest DB

--- Inventory System/Supplier.aspx.designer.cs
+++ Inventory System/Supplier.aspx.designer.cs
@@ -211,13 +211,13 @@ namespace Inventory_System {
         protected global::System.Web.UI.WebControls.DropDownList ddlItemType;
         
         /// <summary>
-        /// SupplierNameRequiredFieldValidator5 control.
+        /// CategoryValidation control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator SupplierNameRequiredFieldValidator5;
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator CategoryValidation;
         
         /// <summary>
         /// btnSave control.
