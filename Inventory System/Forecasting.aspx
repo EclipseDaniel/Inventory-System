7927372c99c98358fa56a41080cf6cf8b42ea02d
leonardmontano <montano.leonard@gmail.com>
2019-10-02 22:16:35 +0800

    Forecasting

---
+++ Inventory System/Forecasting.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Forecasting.aspx.cs" Inherits="Inventory_System.Forecasting" %>
+
+<asp:Content ID="ForecastingHeaderContent" ContentPlaceHolderID="HeaderContent" runat="server">
+    <link href="css/newStyle.css" rel="stylesheet" />
+    <script src="Scripts/Chart.js"></script>
+</asp:Content>
+<asp:Content ID="ForecastingBodyContent" ContentPlaceHolderID="BodyContent" runat="server">
+
+    <div class="spacer"></div>
+
+    <div class="panel panel-success">
+        <div class="panel-heading">
+            <h3 class="panel-title">Forecasting</h3>
+        </div>
+
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-centered">
+                    <asp:Literal ID="ltChart" runat="server"></asp:Literal>
+                </div>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-4">
+                    <label for="ddlMonth" runat="server" class="control-label">Month:</label>
+                    <asp:DropDownList ID="ddlMonth" runat="server" CssClass="form-control">
+                        <asp:ListItem Text="" Value="" Selected="True"></asp:ListItem>
+                        <asp:ListItem Text="January" Value="January"></asp:ListItem>
+                        <asp:ListItem Text="February" Value="February"></asp:ListItem>
+                        <asp:ListItem Text="March" Value="March"></asp:ListItem>
+                        <asp:ListItem Text="April" Value="April"></asp:ListItem>
+                        <asp:ListItem Text="May" Value="May"></asp:ListItem>
+                        <asp:ListItem Text="June" Value="June"></asp:ListItem>
+                        <asp:ListItem Text="July" Value="July"></asp:ListItem>
+                        <asp:ListItem Text="August" Value="August"></asp:ListItem>
+                        <asp:ListItem Text="September" Value="September"></asp:ListItem>
+                        <asp:ListItem Text="October" Value="October"></asp:ListItem>
+                        <asp:ListItem Text="November" Value="November"></asp:ListItem>
+                        <asp:ListItem Text="December" Value="December"></asp:ListItem>
+                    </asp:DropDownList>
+                </div>
+                <div class="col-lg-4">
+                    <label runat="server" class="control-label">Percentage:</label>
+                    <asp:TextBox ID="txtPercentage" runat="server" CssClass="form-control" max="3" placeholder="Enter percentage in decimal(e.g.  .1)"></asp:TextBox>
+                </div>
+                <div class="col-lg-1">
+                    <asp:Button runat="server" ID="btnSavePercentage" CssClass="btn btn-success" Text="Save" Style="margin-top:20px" OnClick="btnSavePercentage_Click" />
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ Inventory System/Forecasting.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Inventory_System
+{
+    public partial class Forecasting : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbMainConnectionString"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                ShowData();
+            }
+        }
+
+        private void ShowData()
+        {
+            
+            string query = "SELECT SupplyRate FROM tblForecasting";
+            SqlCommand cmd = new SqlCommand(query, con);
+            DataTable dt = new DataTable();
+            try
+            {
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                dt.Load(dr, LoadOption.OverwriteChanges);
+                con.Close();
+            }
+            catch
+            {
+
+            }
+
+            if (dt != null)
+            {
+                String chart = "";
+                chart = "<canvas id=\"line-chart\" width=\"100%\" height=\"40\"></canvas>";
+                chart += "<script>";
+                chart += "new Chart(document.getElementById(\"line-chart\"), { type: 'line', data: {labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'";
+                chart += "],datasets: [{ data: [";
+
+                // get data from database and add to chart
+                String value = "";
+                for (int i = 0; i < dt.Rows.Count; i++)
+                    value += dt.Rows[i]["SupplyRate"].ToString() + ",";
+                value = value.Substring(0, value.Length - 1);
+                chart += value;
+
+                chart += "],label: \"Percentage\",borderColor: \"#3e95cd\",fill: true}"; // Chart color
+                chart += "]},options: { title: { display: true,text: 'Inventory Forecasting'} }"; // Chart title
+                chart += "});";
+                chart += "</script>";
+
+                ltChart.Text = chart;
+            }
+        }
+
+        protected void btnSavePercentage_Click(object sender, EventArgs e)
+        {
+            if(con.State == ConnectionState.Closed)
+            {
+                con.Open();
+                string month = ddlMonth.SelectedValue.ToString();
+                string percentage = txtPercentage.Text.ToString();
+                SqlDataAdapter da = new SqlDataAdapter("UPDATE tblForecasting SET SupplyRate= " + percentage + " WHERE Months='" + month + "'", con);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                Response.Redirect("~/Forecasting.aspx");
+            }
+        }
+    }
+}
